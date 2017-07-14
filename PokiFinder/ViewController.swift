f280cb8933541358d9719e424ab28f8cfdc955d3
Badr Moh <badrmoh@badrs-macbook-pro.local>
2017-07-14 14:32:29 +0200

    Initial Commit

---
+++ PokiFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokiFinder
+//
+//  Created by Badr Moh on 14.07.17.
+//  Copyright © 2017 Badr Moh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
